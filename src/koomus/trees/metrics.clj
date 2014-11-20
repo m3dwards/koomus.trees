afde4c5eb74be130be328f7b1af67cc021b38069
Max Edwards <maxedwar@thoughtworks.com>
2014-11-20 22:31:50 +0000

    happy little refactor

--- src/koomus/trees/metrics.clj
+++ src/koomus/trees/metrics.clj
@@ -1,38 +1,46 @@
-(ns koomus.trees.metrics
+(ns bulk-loader.metrics
   (:require 
     [com.stuartsierra.component :as component]
-    [environ.core :as environ]
     [metrics.jvm.core :refer [instrument-jvm]]
     [metrics.reporters.graphite :as graphite]
-    [metrics.core :refer [new-registry]]
-  )
+    [metrics.core :refer [new-registry]])
   (:import 
     [java.util.concurrent TimeUnit]
-    [com.codahale.metrics MetricFilter]
-    )
-  )
+    [com.codahale.metrics MetricFilter]))
+
+(defn- generate-reporter
+  [reg host]
+  (graphite/reporter reg {:host host
+                          :prefix "koomus-metrics"
+                          :rate-unit TimeUnit/SECONDS
+                          :duration-unit TimeUnit/MILLISECONDS
+                          :filter MetricFilter/ALL}))
+
+(defn- init-reporter
+  [{:keys [host]}]
+  (let [reg (new-registry)]
+        (instrument-jvm reg)
+        (let [reporter (generate-reporter reg host)]
+          (graphite/start reporter 10)
+          reporter)))
+
+(defn- start
+  [{:keys [gr] :as this}]
+  (if gr
+      (do (graphite/start gr 10) this)
+      (->> this init-reporter (assoc this :gr))))
+
+(defn- stop
+  [{:keys [gr] :as this}]
+  (graphite/stop gr)
+  this)
 
 (defrecord Metrics [host]
   component/Lifecycle
   (start [this]
-    (if (contains? this :gr)
-      (do (graphite/start (:gr this) 10)
-       this
-       )
-      (do (let [reg (new-registry)]
-        (instrument-jvm reg)
-        (let [reporter (graphite/reporter reg
-                        {:host host
-                        :prefix "koomus-metrics"
-                        :rate-unit TimeUnit/SECONDS
-                        :duration-unit TimeUnit/MILLISECONDS
-                        :filter MetricFilter/ALL})]
-          (graphite/start reporter 10) 
-          (assoc this :gr reporter)))))
-    )
+    (start this))
   (stop [this]
-    (graphite/stop (:gr this))
-    this))
+    (stop this)))
 
 (defn new-metrics  [host]
-  (map->Metrics  {:host host}))
+  (map->Metrics {:host host}))
