752fb47172dc6cbfa52b1838170f526c6c209dfb
Max Edwards <maxedwar@thoughtworks.com>
2014-11-22 13:05:47 +0000

    first test is taking form

--- test/koomus/trees/metrics_test.clj
+++ test/koomus/trees/metrics_test.clj
@@ -1,6 +1,12 @@
 (ns koomus.trees.metrics-test
   (:require [clojure.test :refer :all]
-            [com.stuartsierra.component :as component]))
+            [com.stuartsierra.component :as component]
+            [metrics.core :refer [new-registry]]
+            [koomus.trees.metrics :as metrics]
+            ))
 
-(deftest test-jvm-metrics
-  (is (= 4 (+ 2 2))))
+(deftest test-generate-reporter
+  (let [host "127.0.0.1" reg (new-registry)]
+    (#'metrics/generate-reporter reg host)
+    (println reg)
+    (is true)))
