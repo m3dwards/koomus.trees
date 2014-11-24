5f7715f0379cefcca9a77c2b75c11a7625b5fadd
Max Edwards <maxedwar@thoughtworks.com>
2014-11-24 21:25:41 +0000

    a failing test. I will have to write some code to make it pass

--- test/koomus/trees/metrics_test.clj
+++ test/koomus/trees/metrics_test.clj
@@ -16,11 +16,11 @@
   
   (fact "generate-reporter should return a new graphite reporter" 
     (let [host "127.0.0.1" reg (new-registry)]
-      (generate-reporter reg host) => irrelevant
+      (generate-reporter reg host prefix) => irrelevant
       (provided 
         (graphite/reporter 
           reg {:host host
-               :prefix "koomus-metrics"
+               :prefix prefix
                :rate-unit TimeUnit/SECONDS
                :duration-unit TimeUnit/MILLISECONDS
                :filter MetricFilter/ALL})  => irrelevant)))
