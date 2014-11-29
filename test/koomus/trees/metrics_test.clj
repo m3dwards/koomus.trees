f678b289538cf7be64ab092575a5c050a410f82e
Max Edwards <maxedwar@thoughtworks.com>
2014-11-29 23:29:55 +0000

    It's the types !

--- test/koomus/trees/metrics_test.clj
+++ test/koomus/trees/metrics_test.clj
@@ -13,6 +13,17 @@
 (def generate-reporter #'metrics/generate-reporter)
 (def init-reporter #'metrics/init-reporter)
 
+;;notice this typed param! try removing the type and see the test pass
+(defn typed-params
+  [^long something]
+  (println something)
+  )
+
+(defn run-this
+  [something]
+  (typed-params something)
+  )
+
 (facts "Metrics tests"
   
   (fact "generate-reporter should return a new graphite reporter" 
@@ -34,19 +45,28 @@
   ;;    (graphite/start anything anything) => irrelevant
   ;;    (generate-reporter anything anything) => irrelevant
   ;;    (graphite/reporter anything anything) => irrelevant
-  ;;    )) 
+  ;;    ))
 
+  ;;This works
   (fact "test thing"
     (metrics/test-thing anything) => irrelevant
     (provided 
       (graphite/reporter anything anything) => irrelevant
       )) 
   
-  (fact "other test thing"
-    (metrics/test-other-thing anything) => irrelevant
+  ;;This does not work but it looks exactly like the above
+  ;;(fact "other test thing"
+  ;;  (metrics/test-other-thing anything) => irrelevant
+  ;;  (provided 
+  ;;    (graphite/start anything anything)
+  ;;    ))
+
+  (fact "test thing that might work"
+    (run-this anything) => irrelevant
     (provided 
-      (graphite/start anything anything) => irrelevant
+      (typed-params anything) => irrelevant
       )) 
+
 
 )
 
